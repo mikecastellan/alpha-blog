9b963cfd7aa970cd389891723e43432316729585
Mike Short <mike.short@castellanbc.com>
2021-05-06 10:15:25 +0100

    Edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+	before_action :set_user, only: [:show, :edit, :update, :destroy]
 
 	def new
 		@user = User.new
@@ -14,9 +15,24 @@ class UsersController < ApplicationController
 		end
 	end
 
+	def edit
+	end
+
+	def update
+		if @user.update(user_params)
+			flash[:notice] = 'User was updated successfully'
+			redirect_to articles_path
+		else
+			render 'edit'
+		end
+	end
+
 	private
 	def user_params
 		params.require(:user).permit(:username,:email,:password)
+	end
+	def set_user
+		@user = User.find(params[:id])
 	end
 
 end
