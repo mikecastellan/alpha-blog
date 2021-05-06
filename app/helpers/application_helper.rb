04abb1c19e362ff19f7826931843ffa1028dc806
Mike Short <mike.short@castellanbc.com>
2021-05-06 10:30:07 +0100

    Add users show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+	def gravatar_for(user)
+		email = user.email.downcase
+		hash = Digest::MD5.hexdigest(email)
+		gurl = "https://www.gravatar.com/avatar/#{hash}"
+		image_tag(gurl, alt: user.username)
+	end
 end
